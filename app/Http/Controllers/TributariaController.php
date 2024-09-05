b81c445198626a4fdc0d2d94c65a91d6a23911b8
gonpineiro <gon.pineiro@gmail.com>
2024-09-05 20:02:04 -0300

    test

--- app/Http/Controllers/TributariaController.php
+++ app/Http/Controllers/TributariaController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserPagoOnline;
 use Illuminate\Http\Request;
-use MercadoPago\Client\Preference\PreferenceClient;
-use SoapClient;
-use SoapFault;
 
+use MercadoPago\Client\Preference\PreferenceClient;
 use MercadoPago\MercadoPagoConfig;
 
 
@@ -24,11 +23,11 @@ class TributariaController extends Controller
             ];
 
             try {
-                $client = new SoapClient(null, $options);
+                $client = new \SoapClient(null, $options);
 
                 if ($request->type === 'SW_EmitirCompPagoDeudaVenc' || $request->type === 'SW_EmitirCompPagoNoVenc') {
                     /*  return $this->getXMLRequestEmitirCompPagoDeudaVenc($request); */
-                    $response =  $client->__doRequest($this->getXMLRequestEmitirCompPagoDeudaVenc($request), $options['location'], 'urn:uSWConsultaTributariaIntf-ISWConsultaTributaria#SW_ConsultarDeuda', 1);
+                    $response =  $client->__doRequest($this->getXMLRequestEmitirComp($request), $options['location'], 'urn:uSWConsultaTributariaIntf-ISWConsultaTributaria#SW_ConsultarDeuda', 1);
                 } else {
                     $response =  $client->__doRequest($this->getXMLRequest($request), $options['location'], 'urn:uSWConsultaTributariaIntf-ISWConsultaTributaria#SW_ConsultarDeuda', 1);
                 }
@@ -38,19 +37,23 @@ class TributariaController extends Controller
                     // El contenido extraído está escapado, desescaparlo
                     $xmlContent = htmlspecialchars_decode($matches[1]);
 
-                    if ($request->type === 'SW_EmitirCompPagoDeudaVenc') {
+                    if ($request->type === 'SW_EmitirCompPagoDeudaVenc' && !$request->pago_online) {
                         return sendResponse($xmlContent);
                     }
                     // Cargar el XML limpio
-                    $xmlObject = simplexml_load_string($xmlContent);
+                    $xmlObject = (object)simplexml_load_string($xmlContent);
 
                     if ($xmlObject === false) {
                         return sendResponse(null, 'Failed loading XML', 301);
                     }
 
+                    if ($request->pago_online) {
+                        return $this->pagar_online($xmlObject);
+                    }
+
                     return sendResponse($xmlObject);
                 }
-            } catch (SoapFault $e) {
+            } catch (\SoapFault $e) {
                 return sendResponse(null, $e->getMessage(), 301);
             }
         } catch (\Exception $e) {
@@ -90,10 +93,12 @@ class TributariaController extends Controller
         return $xml->asXML();
     }
 
-    private function getXMLRequestEmitirCompPagoDeudaVenc(Request $request)
+    private function getXMLRequestEmitirComp(Request $request)
     {
         $attributes = $request->params;
         $key = env('KEY_TRIBUTARIA');
+
+        $pagoOnline = $request->pago_online ? $request->pago_online : '';
 
         $type = $request->type;
         $xml = new \SimpleXMLElement("
@@ -110,7 +115,9 @@ class TributariaController extends Controller
                         <VFPDATA>
                         </VFPDATA>
                     </XMLGrupoNroComp>
-                    <Key xsi:type='xsd:string'>{$key}</Key>
+                    <Key xsi:type='xsd:string'>$key</Key>
+                    <pPagoElectronico xsi:type='xsd:string'>$pagoOnline</pPagoElectronico>
+                    <pIpCliente xsi:type='xsd:string'>127.0.0.1</pIpCliente>
                 </urn:$type>
             </soapenv:Body>
         </soapenv:Envelope>");
@@ -138,25 +145,58 @@ class TributariaController extends Controller
         return $xml->asXML();
     }
 
-    public function pagar_online(Request $request)
+    private function pagar_online($operacion)
     {
-        /* Test */
-        MercadoPagoConfig::setAccessToken('');
+        try {
+            $operacion = json_decode(json_encode($operacion), true);
 
-        $preference = new PreferenceClient();
+            $opid = $operacion['opid'];
+            $nro_comprobante = str_replace('/', '-', $operacion["comprobantes"]["comprobante"]["nro_comprobante"]);
+
+            UserPagoOnline::create([
+                'opid' => $opid,
+                'nro_comprobante' => $nro_comprobante,
+                'comprobante' => json_encode($operacion["comprobantes"]["comprobante"]),
+            ]);
+            return $this->pagar_online_mp($opid);
+        } catch (\Exception $e) {
+            return sendResponse(null, $e->getMessage(), 301);
+        }
+    }
+
+    private function pagar_online_mp($opid)
+    {
+        /* $operacion = $request->operacion; */
+        $userPagoOnline = UserPagoOnline::where('opid', $opid)->first();
+        $comprobante = json_decode($userPagoOnline->comprobante);
+
+        $preferencia = [
+            "external_reference" => $opid,
+            "items" => [
+                [
+                    "title" =>  $userPagoOnline->nro_comprobante,
+                    "quantity" => 1,
+                    "unit_price" => $this->format_importe($comprobante->importe)
+                ]
+            ]
+        ];
+
+        MercadoPagoConfig::setAccessToken(env('MP_PRIVATE_TOKEN'));
 
         $client = new PreferenceClient();
-        $preference = $client->create([
-            "external_reference" => "6ZL0SBWDT20240905000047584433",
-            "items" => array(
-                array(
-                    "title" => str_replace('/', '-', '11/002908598/87'), // 11-002908598-87
-                    "quantity" => 1,
-                    "unit_price" => 1743345
-                )
-            )
-        ]);
+        $preference = $client->create($preferencia);
+
+        $userPagoOnline->mp_preference_id = $preference->id;
+        $userPagoOnline->mp_preference = json_encode($preference);
+        $userPagoOnline->save();
 
         return sendResponse($preference);
+    }
+
+    protected function format_importe($importe)
+    {
+        $numero = (int)$importe;
+
+        return $numero / 100;
     }
 }
---
+++ app/Models/UserPagoOnline.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserPagoOnline extends Model
+{
+    protected $table = 'user_pago_online';
+    protected $fillable = ['user_id', 'opid', 'nro_comprobante', 'comprobante'];
+}
